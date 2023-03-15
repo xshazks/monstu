056697f2d1e5837efd6798882c336364b4f3946c
Rolly Maulana Awangga <rolly@awang.ga>
2023-03-16 06:13:31 +0700

    add new

--- url/url.go
+++ url/url.go
@@ -11,4 +11,7 @@ func Web(page *fiber.App) {
 	page.Post("/api/whatsauth/request", controller.PostWhatsAuthRequest)  //API from user whatsapp message from iteung gowa
 	page.Get("/ws/whatsauth/qr", websocket.New(controller.WsWhatsAuthQR)) //websocket whatsauth
 
+	page.Get("/", controller.Sink)
+	page.Post("/", controller.Sink)
+
 }
