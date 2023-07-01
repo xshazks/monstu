e1c6c779414315c85414672fc8e092bfcbeb3305
xshazks <shahiezasauki15@gmail.com>
2023-07-01 23:02:51 +0700

    tes

--- url/url.go
+++ url/url.go
@@ -12,7 +12,6 @@ func Web(page *fiber.App) {
 	page.Get("/ws/whatsauth/qr", websocket.New(controller.WsWhatsAuthQR)) //websocket whatsaut
 	page.Get("/", controller.GetHome)
 	page.Get("/dafdir", controller.GetDataDafdir)
-	page.Get("/dafdir", controller.GetDataDafdir)
 	page.Get("/nilai", controller.GetDataNilai)
 	page.Get("/pelanggar", controller.GetDafpel)
 	page.Get("/bayar", controller.GetDataPembayaran)
