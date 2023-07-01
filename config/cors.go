b2e4657bda8a9d3dac155c8aa3a4d53ef3693f0c
xshazks <shahiezasauki15@gmail.com>
2023-07-01 23:59:44 +0700

    br

--- config/cors.go
+++ config/cors.go
@@ -10,12 +10,19 @@ import (
 var origins = []string{
 	"https://auth.ulbi.ac.id",
 	"https://sip.ulbi.ac.id",
+	"http://127.0.0.1:5500",
+	"http://127.0.0.1:5501",
+	"http://127.0.0.1:5502",
+	"http://127.0.0.1:5503",
 	"https://euis.ulbi.ac.id",
 	"https://home.ulbi.ac.id",
 	"https://alpha.ulbi.ac.id",
 	"https://dias.ulbi.ac.id",
 	"https://iteung.ulbi.ac.id",
 	"https://whatsauth.github.io",
+	"https://xshazks.github.io",
+	"https://gocroot.github.io/",
+	"https://monstu-20284020e86e.herokuapp.com",
 }
 
 var Internalhost string = os.Getenv("INTERNALHOST") + ":" + os.Getenv("PORT")
